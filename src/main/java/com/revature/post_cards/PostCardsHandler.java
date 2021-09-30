bb42aa33511947980dbd94882b08ff10219b0b93
Bill Thomas <willoney@gmail.com>
2021-09-30 15:33:23 -0500

    refactored by cleaning up things. deleted all files and code lines that where not used.
    Signed-off-by: Bill Thomas <willoney@gmail.com>

--- src/main/java/com/revature/post_cards/PostCardsHandler.java
+++ src/main/java/com/revature/post_cards/PostCardsHandler.java
@@ -8,56 +8,22 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.MalformedJsonException;
 import com.revature.documents.*;
-import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public class PostCardsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    private final TagRepository tagRepo;
     private final SetRepository setRepo;
-    private final UserRepository userRepo;
 
     private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
 
     public PostCardsHandler(){
-        this.userRepo = new UserRepository();
         this.setRepo = new SetRepository();
-        this.tagRepo = new TagRepository();
     }
 
-    public PostCardsHandler(TagRepository tagRepo, SetRepository setRepo, UserRepository userRepo){
-        this.userRepo = new UserRepository();
+    public PostCardsHandler(SetRepository setRepo){
         this.setRepo = new SetRepository();
-        this.tagRepo = new TagRepository();
     }
-/*
-//////////////////////////////////////////////////////
-    public APIGatewayProxyResponseEvent getOldSet(APIGatewayProxyRequestEvent requestEvent, Context context){
-
-        LambdaLogger logger = context.getLogger();
-        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
-
-        logger.log("Deployment successful.");
-
-        Map<String, String> queryParams = requestEvent.getQueryStringParameters();
-
-        PageIterable<Set> sets;
-
-        sets = setRepo.searchSets(queryParams, logger);
-
-        List<SetResponse> respBody = setService.mapResponse(sets, logger);
-        responseEvent.setBody(mapper.toJson(respBody));
-        responseEvent.setStatusCode(200);
-
-        return responseEvent;
-
-    }
-    //////////////////////////////////////////////////////*/
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
@@ -94,29 +60,9 @@ public class PostCardsHandler implements RequestHandler<APIGatewayProxyRequestEv
 
         setRepo.addSet(set);
 
-        /*User author = userRepo.getUser(oldSet.getAuthor());
-
-        newSet.setSet_name(oldSet.getSet_name());
-        newSet.setAuthor(oldSet.getAuthor());
-        newSet.set_public(oldSet.is_public());
-        //TODO add all the fields
-        //TODO add card to card array
-
-        List<Tag> trans_Tags = tagRepo.findTags(oldSet.getTags());
-        logger.log("Tags: " + trans_Tags + "\n");
-
-        newSet.setTags(trans_Tags);
-        newSet.setCards(new ArrayList<Card>());
-
-        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
-
-        newSet = setRepo.addSet(newSet);
-
-        userRepo.addSet(newSet, author);*/
-
-
-
         responseEvent.setBody(mapper.toJson(card));
+
+        System.out.println(responseEvent);
 
         return responseEvent;
 
--- src/main/java/com/revature/post_cards/SetRepository.java
+++ src/main/java/com/revature/post_cards/SetRepository.java
@@ -1,18 +1,12 @@
 package com.revature.post_cards;
 
 
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.revature.documents.Set;
-import lombok.SneakyThrows;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-
-import java.util.*;
 
 public class SetRepository {
 
@@ -26,7 +20,6 @@ public class SetRepository {
 
     public Set addSet(Set updatedSet) {
         System.out.println("updatedSet" + updatedSet);
-        //UUID uuid = UUID.randomUUID();
         setTable.putItem(updatedSet);
         System.out.println("SET WITH ID: " + updatedSet);
         return updatedSet;
@@ -39,41 +32,4 @@ public class SetRepository {
         return result;
     }
 
-
-/*
-    //////////////////////////////////////////////////////
-    @SneakyThrows
-    public PageIterable<Set>  searchSets(Map<String, String> queryParams, LambdaLogger logger) {
-        StringBuilder filterExprBuilder = new StringBuilder();
-        Map<String, AttributeValue> attributeValues = new HashMap<>();
-        List<String> paramKeys = new ArrayList<>(queryParams.keySet());
-
-        List<String> bookFieldNames = Set.getFieldNameStrings();
-
-        for (int i = 0; i < paramKeys.size(); i++){
-
-            filterExprBuilder.append("(");
-            if (i != 0) filterExprBuilder.append(" and ");
-
-            String paramKey = paramKeys.get(i);
-
-            if (!bookFieldNames.contains(paramKey)){
-                String msg = "The field, " + paramKey + ", was not found on resource type: Book";
-                logger.log(msg);
-                throw new RuntimeException(msg);
-            }
-
-            String fieldType = Set.class.getDeclaredField(paramKey).getType().getSimpleName();
-            String paramVal = Optional.ofNullable(queryParams.get(paramKey))
-                    .orElseThrow(() -> {
-                        String msg = "Unexpected null value found in parameter map.";
-                        logger.log(msg);
-                        return new RuntimeException(msg);
-                    });
-
-
-        }
-    }
-    //////////////////////////////////////////////////////
-}*/
 }
